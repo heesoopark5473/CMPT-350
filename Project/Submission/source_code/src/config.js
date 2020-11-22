c1ce6f797ca1a295f5ecef654a363f7835e1f6ab
hep656 <hep656@usask.ca>
2020-11-22 15:06:37 -0600

    proj

--- Project/Submission/source_code/src/config.js
+++ Project/Submission/source_code/src/config.js
@@ -1,9 +1,9 @@
 const CURRENT_YEAR = (new Date()).getFullYear()
 const NEWS_URL = 'http://newsapi.org/v2/top-headlines?';
 const NEWS_KEY = 'db898a9e78a949d68a2aeb1466c43a70';
-//const YOUTUBE_KEY = 'AIzaSyC15q41RjZkuP2Y-DdSGMdGclnYJFoYUFc';
-//const YOUTUBE_KEY = 'AIzaSyCLKw2q_v2osB5uBw6q_eq_ab_EdqZiz7c';
-const YOUTUBE_KEY = 'AIzaSyAiigZJ0wJC0JKnAzgLiSlBKC_Kye9rzUw';
+// const YOUTUBE_KEY = 'AIzaSyC15q41RjZkuP2Y-DdSGMdGclnYJFoYUFc';
+const YOUTUBE_KEY = 'AIzaSyC7ILV8GBqlMsCs1gdNsLfKHJBFeA-XTgg';
+// const YOUTUBE_KEY = 'AIzaSyAiigZJ0wJC0JKnAzgLiSlBKC_Kye9rzUw';
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3';
 
 const query='Covid+News';
